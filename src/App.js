a73871af438e594b7aefeb1708e0c4f1de30ee04
Winston Chan <winstonchan05@gmail.com>
2022-06-11 18:15:58 -0700

    installed libraries created base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+
+import { OrbitControls, Sky } from "@react-three/drei";
+import { Physics, useBox, usePlane } from "@react-three/cannon";
+
+function Block() {
+  const [ref, api] = useBox(() => ({ mass: 1 }));
+  return (
+    <mesh
+      onClick={() => {
+        api.velocity.set(0, 2, 0);
+      }}
+      ref={ref}
+      position={[0, 1, 0]}
+    >
+      <boxBufferGeometry attach='geometry' />
+      <meshLambertMaterial attach='material' color='grey' />
+    </mesh>
+  );
+}
+
+function Ground() {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
+  return (
+    <mesh position={(0, 0, 0)} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach='geometry' args={[100, 100]} />
+      <meshLambertMaterial attach='material' color='green' />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <OrbitControls />
+      <Sky />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Physics>
+        <Block />
+        <Ground />
+      </Physics>
+    </Canvas>
   );
 }
 
